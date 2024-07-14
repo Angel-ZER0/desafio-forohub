b58172bb41ffd6865bf44bf127a816acb253ae3a
Angel-ZER0 <cora.angelandres@gmail.com>
2024-07-14 14:49:34 -0600

    clase que ejecuta el método main para ejecutar el proyecto

---
+++ src/main/java/angel_zero/desafios_aluraONE/ForoHub/ForoHubApplication.java
@@ -0,0 +1,13 @@
+package angel_zero.desafios_aluraONE.ForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubApplication.class, args);
+	}
+
+}
